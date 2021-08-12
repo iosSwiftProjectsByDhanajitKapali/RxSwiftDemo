cd08922adab865563508edc7a9b5b0bdf1f033c3
Dhanajit kapali <dhanajitkapali30@gmail.com>
2021-08-12 15:46:52 +0530

    added code to create a observable, and bind it to a tableView

--- RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/ViewController.swift
@@ -6,11 +6,29 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    //Creating a observable, will work as a datasource for the tableVIew
+    let tableViewItems = Observable.just(["Item 1","Item 2", "Item 3", "Item 4" ])
+    
+    //creating a dispose bag
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //bind the data to the tableView
+        tableViewItems.bind(to: myTableView
+                                .rx
+                                .items(cellIdentifier: "myCell")){
+            (tableView, tableViewData, cell) in
+            cell.textLabel?.text = tableViewData
+        }.disposed(by: disposeBag)
         
     }
 
