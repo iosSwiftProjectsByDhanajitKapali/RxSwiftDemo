211ae6397a1cd91951f51f7b91f3ca706cc1fb44
Dhanajit kapali <dhanajitkapali30@gmail.com>
2021-08-12 18:29:59 +0530

    added code to query from the tableView dataSource, used map and filter to achieve it.

--- RxSwiftDemo/Controllers/ViewController.swift
+++ RxSwiftDemo/Controllers/ViewController.swift
@@ -11,34 +11,39 @@ import RxCocoa
 
 class ViewController: UIViewController {
     
-    //create the Observable, will work as datasource for myTableView
-    let tableViewItems = Observable.just([MyProduct(name: "PaperPlane", image: "paperplane"),
-                                          MyProduct(name: "PaperPlabe", image: "paperplane.fill"),
-                                          MyProduct(name: "Trash", image: "trash"),
-                                          MyProduct(name: "Trash", image: "trash.fill"),
-                                          MyProduct(name: "Calendar", image: "calendar"),
-                                          MyProduct(name: "Calendar", image: "calendar.circle.fill"),
-                                          MyProduct(name: "Book", image: "book"),
-                                          MyProduct(name: "Book", image: "book.fill"),
-                                          MyProduct(name: "Person", image: "person"),
-                                          MyProduct(name: "Person", image: "person.fill")])
+    //making it a BehaviourRelay, will work as datasource for myTableView
+    let tableViewItems = BehaviorRelay.init(value: [MyProduct(name: "PaperPlane", image: "paperplane"),
+                                                    MyProduct(name: "PaperPlabe", image: "paperplane.fill"),
+                                                    MyProduct(name: "Trash", image: "trash"),
+                                                    MyProduct(name: "Trash", image: "trash.fill"),
+                                                    MyProduct(name: "Calendar", image: "calendar"),
+                                                    MyProduct(name: "Calendar", image: "calendar.circle.fill"),
+                                                    MyProduct(name: "Book", image: "book"),
+                                                    MyProduct(name: "Book", image: "book.fill"),
+                                                    MyProduct(name: "Person", image: "person"),
+                                                    MyProduct(name: "Person", image: "person.fill")])
     
     //create a dispose bag
     let disposeBag = DisposeBag()
     
     
     @IBOutlet var myTableView: UITableView!
+    @IBOutlet var mySearchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "MyProducts"
         
-        //bind the tableViewItems to the myTableView
-        tableViewItems.bind(to: myTableView
-                                .rx
-                                .items(cellIdentifier: "myCell")){
-                (tableView, tableViewData, cell) in
+        //Getting the String from the SearchBar
+        _ = mySearchBar.rx.text.orEmpty.throttle(.milliseconds(300), scheduler: MainScheduler.instance).distinctUntilChanged().map ({ query in
+            self.tableViewItems.value.filter ({ myProduct in
+                query.isEmpty || myProduct.name.lowercased().contains(query.lowercased())
+            })
+        }).bind(to: myTableView
+                    .rx
+                    .items(cellIdentifier: "myCell")){
+            (tableView, tableViewData, cell) in
             cell.textLabel?.text = tableViewData.name
             cell.imageView?.image = UIImage(systemName: tableViewData.image)
         }.disposed(by: disposeBag)
@@ -49,7 +54,7 @@ class ViewController: UIViewController {
             myProductDetailsVC.myProductImageName.accept(myProductObject.image)
             self.navigationController?.pushViewController(myProductDetailsVC, animated: true)
         }).disposed(by: disposeBag)
-
+        
         
     }
     
