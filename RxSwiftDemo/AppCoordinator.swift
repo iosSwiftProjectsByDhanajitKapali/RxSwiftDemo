8c1950223c10bdd525db63f681d65a8d3ae3ca72
Dhanajit kapali <dhanajitkapali30@gmail.com>
2021-08-13 17:40:54 +0530

    added coordinator

---
+++ RxSwiftDemo/AppCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  AppCoordinator.swift
+//  RxSwiftDemo
+//
+//  Created by unthinkable-mac-0025 on 13/08/21.
+//
+
+import UIKit
+
+class AppCoordinator{
+    
+    private let window : UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start(){
+        let viewController = ViewController.instantiate()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
--- RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/ViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    static func instantiate() -> ViewController{
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let viewController = storyboard.instantiateInitialViewController() as! ViewController
+        return viewController
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
     }
 
